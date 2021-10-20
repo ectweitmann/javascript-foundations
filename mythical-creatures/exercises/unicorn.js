0280bfb40e95ee08ead3407debe11f116d795d90
Ethan Tweitmann <ethantweitmann@gmail.com>
2021-10-19 17:54:20 -0700

    Add finished Unicorn class

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,22 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
+    if (color === undefined) {
+      this.color = "white";
+    } else {
     this.color = color;
+    }
+  }
+  isWhite(color) {
+    if (color !== "white") {
+      return false;
+    }
+  }
+  says(phrase) {
+    return `**;* ${phrase} *;**`;
   }
 }
+
+
 
 module.exports = Unicorn;
