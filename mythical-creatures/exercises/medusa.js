6868faf53bdc46dfbc2b27c1ace1472f3d122549
Ethan Tweitmann <ethantweitmann@gmail.com>
2021-10-19 17:57:54 -0700

    Add finished Medusa class

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,25 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+  constructor(name) {
+    this.name = name;
+    this.statues = [];
+  }
+  gazeAtVictim(Person) {
+    if (this.statues.length < 3) {
+      var newStatue = new Statue(Person.name);
+      this.statues.push(newStatue);
+    } else if (this.statues.length = 3) {
+      var newStatue = new Statue(Person.name);
+      this.statues.push(newStatue);
+      var freePerson = Object.assign(Person, this.statues[0]);
+      freePerson.mood = "relieved";
+      this.statues.shift();
+      return freePerson;
+    }
+  }
+}
+
+
+module.exports = Medusa;
