cde04e50cfd5d6a2dd759d7e54bd2f7f0ca25607
Ethan Tweitmann <ethantweitmann@gmail.com>
2021-10-19 17:55:10 -0700

    Add finished Dragon class

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,24 @@
+class Dragon {
+  constructor(name, rider) {
+    this.name = name;
+    this.hungry = true;
+    this.mealCounter = 1;
+    this.rider = rider;
+  }
+  greet() {
+    return `Hi, ${this.rider}!`;
+  }
+  eat() {
+    if (this.mealCounter < 3) {
+      this.hungry = true;
+      this.mealCounter += 1;
+    } else {
+      this.hungry = false;
+    }
+  }
 
+}
+
+
+
+module.exports = Dragon;
